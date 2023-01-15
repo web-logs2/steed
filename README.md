71fe6f8d997405e4300c132c39e318a5cd98676e
Yi Yang <qingfeng.yy@alibaba-inc.com>
2023-01-15 20:24:18 +0800

    feat: use lambda

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Steed
-<img src="logo.png?raw=true" align="right" alt="" weight="120" height="120"/>Hello, this is a free and complete lisp implementation, most of my Lisp knowledge comes from Practical Common List book,
+<img src="logo.png?raw=true" align="right" alt="" weight="120" height="120"/>Hello, this is a free and complete lisp implementation, most of my Lisp knowledge comes from [Practical Common List](https://gigamonkeys.com/book/) book,
 and some old memories. All of these make up my impression on Lisp.
 The main reference manual is that book which contains core design and core concepts of Lisp, I will re-design and implement the parts that do not suit my taste.
 
@@ -13,5 +13,11 @@ The skeleton of a function definitio looks like this:
 ```
 Where `*` means you can create sequential ones.
 
+## Lambda
+When it seems like overkill to define a new function with DEFUN, you can create an "anonymous" function using a lambda expression
+```
+((lambda ( x y) (+ x y)) 2 3)
+```
+You can imagine that *Lambda* is name of this anonymous function, it works exactly the same as normal function.
 # Progress
 It works in progress as I'm read Practical Common List now, you can also contributed your awesome code if you are interested in it.
--- steed.py
+++ steed.py
@@ -15,6 +15,7 @@ import os
 import re
 import string
 import sys
+from random import random
 
 TheBuiltin = [
     {'name': "format", 'param': ['fmt', "args"], 'body': lambda ctxs, args: print(args['fmt'].format(args['args']))},
@@ -26,11 +27,7 @@ TheBuiltin = [
 ]
 
 
-def log_trace(a, b):
-    print(f"== Evaluate S-expression {a} that produces {b}({type(b)})")
-
-
-def make_sexpr(text):
+def make_sexpr_list(text):
     """
     convert source code to s-expressions and make s-expressions to python
     objects to facilitate further use
@@ -114,81 +111,116 @@ def make_sexpr(text):
     return sexprs
 
 
-def eval_sexpr_list(contexts, sexprs):
-    def eval_sexpr(sexpr):
-        if re.match("\".*\"", sexprs) or re.match("-?[0-9][0-9.]]*", sexprs):
-            return eval(sexprs)
-        else:
-            for var in contexts[-1]['var']:
-                if var['name'] == sexprs:
-                    return var['value']
-        raise ValueError(f"Unknown single s-expression {sexprs}")
+class Evaluator:
+    def __init__(self, sexprs):
+        self.sexprs = sexprs
+        self.contexts = [{"func": [func for func in TheBuiltin], "var": []}]
 
-    if type(sexprs) is not list:
-        val = eval_sexpr(sexprs)
-        log_trace(sexprs, val)
-        return val
-
-    if len(sexprs) == 0:
-        log_trace(sexprs, None)
+    def find_func(self, name):
+        for i in reversed(range(len(self.contexts))):
+            for func in self.contexts[i]['func']:
+                if func['name'] == name:
+                    return func
         return None
 
-    action = sexprs[0]
-    if action == 'quote' or action == '\'':
-        # quote
-        log_trace(sexprs, sexprs)
-        return sexprs
-    elif action == 'def':
-        # function definition
-        func = {'name': sexprs[1], 'param': sexprs[2], 'body': sexprs[3:]}
-        contexts[-1]['func'].append(func)
-        log_trace(sexprs, func)
-        return func
-    else:
-        def find_func(ctxs, name):
-            for ctx_idx in reversed(range(len(ctxs))):
-                for func in contexts[ctx_idx]['func']:
-                    if func['name'] == action:
-                        return func
-            return None
-
-        # general identifier, either a use of var or function call
-        for var in contexts[-1]['var']:
-            if var['name'] == action:
-                log_trace(sexprs, var['value'])
+    def find_var(self, name):
+        for var in self.contexts[-1]['var']:
+            if var['name'] == name:
+                Evaluator.log_trace(sexprs, var['value'])
                 return var['value']
 
-        func = find_func(contexts, action)
-        if func is not None:
-            # Prepare arguments
-            new_ctx = {"func": [], "var": []}
+    def call(self, func, sexprs):
+        if len(func['param']) != len(sexprs) - 1:
+            raise ValueError(
+                f"Function {func['name']} expects {len(func['param'])} parameter but found {len(sexprs) - 1}")
+        # Prologue, prepare arguments
+        new_ctx = {"func": [], "var": []}
+        for i in range(0, len(func['param'])):
+            new_ctx['var'].append({"name": func['param'][i], "value": self.eval_sexpr_list(sexprs[i + 1])})
 
-            if len(func['param']) != len(sexprs) - 1:
-                raise ValueError(
-                    f"Function {func['name']} expects {len(func['param'])} parameter but found {len(sexprs) - 1}")
+        # Call builtin or user defined function
+        body = func['body']
+        ret_val = None
+        self.contexts.append(new_ctx)
+        if callable(body):
+            args = {}
+            for item in new_ctx['var']:
+                args[item['name']] = item['value']
+            ret_val = body(self.contexts, args)
+        else:
+            for i in range(len(body)):
+                var = self.eval_sexpr_list(body[i])
+                if i == len(body) - 1:
+                    ret_val = var
+        del self.contexts[-1]
 
-            for i in range(0, len(func['param'])):
-                new_ctx['var'].append({"name": func['param'][i], "value": eval_sexpr_list(contexts, sexprs[i + 1])})
+        Evaluator.log_trace(sexprs, ret_val)
+        return ret_val
 
-            # Call builtin or user defined function
-            body = func['body']
-            ret_val = None
-            contexts.append(new_ctx)
-            if callable(body):
-                args = {}
-                for item in new_ctx['var']:
-                    args[item['name']] = item['value']
-                ret_val = body(contexts, args)
-            else:
-                for i in range(len(body)):
-                    var = eval_sexpr_list(contexts, body[i])
-                    if i == len(body) - 1:
-                        ret_val = var
-            del contexts[-1]
+    def eval_top_level(self):
+        for i in sexprs:
+            val = self.eval_sexpr_list(i)
+            print(val)
 
-            log_trace(sexprs, ret_val)
-            return ret_val
-    raise ValueError("Should not reach here")
+    def eval_sexpr(self, sexpr):
+        if re.match("\".*\"", sexpr) or re.match("-?[0-9][0-9.]*", sexpr):
+            return eval(sexpr)
+        else:
+            for var in self.contexts[-1]['var']:
+                if var['name'] == sexpr:
+                    return var['value']
+        raise ValueError(f"Unknown single s-expression {sexpr}")
+
+    def eval_sexpr_list(self, sexprs):
+        if type(sexprs) is not list:
+            val = self.eval_sexpr(sexprs)
+            Evaluator.log_trace(sexprs, val)
+            return val
+
+        if len(sexprs) == 0:
+            Evaluator.log_trace(sexprs, None)
+            return None
+
+        action = sexprs[0]
+        if type(action) is list:
+            action = self.eval_sexpr_list(action)
+            sexprs[0] = action
+
+        if "body" in action:
+            # ((lambda ...) a b)
+            func = action
+            return self.call(func, sexprs)
+        if action == 'quote' or action == '\'':
+            # quote
+            Evaluator.log_trace(sexprs, sexprs)
+            return sexprs
+        elif action == 'def':
+            # function definition
+            func = {'name': sexprs[1], 'param': sexprs[2], 'body': sexprs[3:]}
+            self.contexts[-1]['func'].append(func)
+            Evaluator.log_trace(sexprs, func)
+            return func
+        elif action == 'lambda':
+            # lambda function definition
+            func = {'name': '<lambda>', 'param': sexprs[1], 'body': sexprs[2:]}
+            self.contexts[-1]['func'].append(func)
+            Evaluator.log_trace(sexprs, func)
+            return func
+        else:
+            # general identifier, either a use of var or function call
+            var = self.find_var(action)
+            if var is not None:
+                return var
+
+            func = self.find_func(action)
+            if func is not None:
+                return self.call(func, sexprs)
+
+        raise ValueError("Should not reach here")
+
+    @staticmethod
+    def log_trace(a, b):
+        print(f"== Evaluate S-expression {a} that produces {b}({type(b)})")
 
 
 if __name__ == '__main__':
@@ -198,11 +230,7 @@ if __name__ == '__main__':
 
     with open(sys.argv[1]) as file:
         content = file.read()
-        sexprs = make_sexpr(content)
+        sexprs = make_sexpr_list(content)
 
-    # register builtin functions
-    contexts = [{"func": [func for func in TheBuiltin], "var": []}]
-
-    # evaluate top-level expressions
-    for i in sexprs:
-        val = eval_sexpr_list(contexts, i)
+    e = Evaluator(sexprs)
+    e.eval_top_level()
